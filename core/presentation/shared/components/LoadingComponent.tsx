1a27d5a96c9fc1077fe259947bd5deb881760583
ben wainaina <bwainainamwangi@gmail.com>
2024-07-06 06:13:32 +0300

    cleanup alert loading component

--- core/presentation/shared/components/LoadingComponent.tsx
+++ core/presentation/shared/components/LoadingComponent.tsx
@@ -1,25 +1,27 @@
-import {Text, View} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import {FONT_POPPINS} from '../utilities/constants/fonts.constants';
 
 export const LoadingComponent = ({message}: {message?: string}) => {
   return (
-    <View
-      style={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'white',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <Text
-        style={{
-          color: 'black',
-          fontSize: 16,
-          fontFamily: FONT_POPPINS.regular,
-        }}>
-        {message}
-      </Text>
+    <View style={STYLES.containerOne}>
+      <Text style={STYLES.textOne}>{message}</Text>
     </View>
   );
 };
+
+const STYLES = StyleSheet.create({
+  containerOne: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textOne: {
+    color: 'black',
+    fontSize: 16,
+    fontFamily: FONT_POPPINS.regular,
+  },
+});
