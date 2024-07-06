377695cccd5c42dfb881fca4b2a95ccc204c1eef
ben wainaina <bwainainamwangi@gmail.com>
2024-07-06 06:15:48 +0300

    cleanup routes

--- core/presentation/shared/routes.tsx
+++ core/presentation/shared/routes.tsx
@@ -1,6 +1,7 @@
-import {useDispatch, useSelector} from 'react-redux';
+import React from 'react';
+import {useSelector} from 'react-redux';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {useState} from 'react';
+
 import {LoadingComponent} from './components/LoadingComponent';
 import {
   selectIsValidatingToken,
@@ -8,27 +9,15 @@ import {
 } from '../../state_manager/shared/selectors';
 import {AuthComponent} from '../auth/AuthComponent';
 import {HomeComponent} from '../home/HomeComponent';
-import {useNavigation} from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
 export const AppRoutes = () => {
   /**
-   * hooks
-   */
-  const dispatch = useDispatch();
-  const navigation = useNavigation();
-
-  /**
    * selectors
    */
   const isValidatingToken = useSelector(selectIsValidatingToken);
   const tokenIsValid = useSelector(selectUserTokenValidity);
-
-  /**
-   * states
-   */
-  const [resolvingRoute, setResolvingRoute] = useState<boolean>(true);
 
   return isValidatingToken || tokenIsValid === undefined ? (
     <LoadingComponent message={'Getting ready...'} />
@@ -36,11 +25,11 @@ export const AppRoutes = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       {tokenIsValid ? (
         <Stack.Group>
-          <Stack.Screen name="home" component={HomeComponent}></Stack.Screen>
+          <Stack.Screen name="home" component={HomeComponent} />
         </Stack.Group>
       ) : (
         <Stack.Group>
-          <Stack.Screen name="auth" component={AuthComponent}></Stack.Screen>
+          <Stack.Screen name="auth" component={AuthComponent} />
         </Stack.Group>
       )}
     </Stack.Navigator>
